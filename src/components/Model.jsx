af7e4c40cae3300641ac722c07ea0b56a8d70064
ieatn <ieatnubcake@gmail.com>
2023-04-26 12:09:43 -0700

    sidebar

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -7,15 +7,24 @@ const Model = () => {
         <h1 className="text-4xl font-bold">Model 3</h1>
         <p className="p-2">
           Order Online for
-          <span className="underline underline-offset-4">
-            Touchless Delivery
-          </span>
+          <a href="https://www.tesla.com/support/taking-delivery?redirect=no">
+            <span className="underline underline-offset-4">
+              Touchless Delivery
+            </span>
+          </a>
         </p>
       </div>
       <div className="flex flex-col items-center justify-center text-sm absolute inset-x-0 bottom-[10%] lg:flex-row">
-        {/* copy bg color for buttons on website using css peeper */}
-        <button className="bg-[#393c41] h-10 rounded-md w-96 mt-2 text-white lg:w-64 lg:mx-4">Custom Order</button>
-        <button className="bg-[#f4f4f4] h-10 rounded-md w-96 mt-2 lg:w-64 lg:mx-4">Existing Inventory</button>
+        <a href="https://www.tesla.com/model3/design">
+          <button className="bg-[#393c41] h-10 rounded-md w-96 mt-2 text-white lg:w-64 lg:mx-4">
+            Custom Order
+          </button>
+        </a>
+        <a href="https://www.tesla.com/model3/design">
+          <button className="bg-[#f4f4f4] h-10 rounded-md w-96 mt-2 lg:w-64 lg:mx-4">
+            Existing Inventory
+          </button>
+        </a>
       </div>
       <div className="flex justify-center animate-bounce absolute inset-x-0 bottom-[3%]">
         <svg
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,133 @@
-import React from "react";
+import React, { useState } from "react";
 
 // learned to format document using prettier very cool
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
     <div className="flex justify-between items-center text-sm font-bold px-12 p-4">
       <div>
-        {/* copy image address using css peeper of logo */}
-        <img
-          className="h-3"
-          src="data:image/svg+xml;base64,PHN2ZyBjbGFzcz0idGRzLWljb24gdGRzLWljb24tbG9nby13b3JkbWFyayB0ZHMtc2l0ZS1sb2dvLWljb24iIHZpZXdCb3g9IjAgMCAzNDIgMzUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTAgLjFhOS43IDkuNyAwIDAgMCA3IDdoMTFsLjUuMXYyNy42aDYuOFY3LjNMMjYgN2gxMWE5LjggOS44IDAgMCAwIDctN0gwem0yMzguNiAwaC02Ljh2MzQuOEgyNjNhOS43IDkuNyAwIDAgMCA2LTYuOGgtMzAuM1Ywem0tNTIuMyA2LjhjMy42LTEgNi42LTMuOCA3LjQtNi45bC0zOC4xLjF2MjAuNmgzMS4xdjcuMmgtMjQuNGExMy42IDEzLjYgMCAwIDAtOC43IDdoMzkuOXYtMjFoLTMxLjJ2LTdoMjR6bTExNi4yIDI4aDYuN3YtMTRoMjQuNnYxNGg2Ljd2LTIxaC0zOHpNODUuMyA3aDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3ptMCAxMy44aDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3ptMCAxNC4xaDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3pNMzA4LjUgN2gyNmE5LjYgOS42IDAgMCAwIDctN2gtNDBhOS42IDkuNiAwIDAgMCA3IDd6IiBmaWxsPSJjdXJyZW50Q29sb3IiLz48L3N2Zz4="
-          alt="Tesla Logo"
-        />
+        <a href="https://www.tesla.com/">
+          <img
+            className="h-3"
+            src="data:image/svg+xml;base64,PHN2ZyBjbGFzcz0idGRzLWljb24gdGRzLWljb24tbG9nby13b3JkbWFyayB0ZHMtc2l0ZS1sb2dvLWljb24iIHZpZXdCb3g9IjAgMCAzNDIgMzUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTAgLjFhOS43IDkuNyAwIDAgMCA3IDdoMTFsLjUuMXYyNy42aDYuOFY3LjNMMjYgN2gxMWE5LjggOS44IDAgMCAwIDctN0gwem0yMzguNiAwaC02Ljh2MzQuOEgyNjNhOS43IDkuNyAwIDAgMCA2LTYuOGgtMzAuM1Ywem0tNTIuMyA2LjhjMy42LTEgNi42LTMuOCA3LjQtNi45bC0zOC4xLjF2MjAuNmgzMS4xdjcuMmgtMjQuNGExMy42IDEzLjYgMCAwIDAtOC43IDdoMzkuOXYtMjFoLTMxLjJ2LTdoMjR6bTExNi4yIDI4aDYuN3YtMTRoMjQuNnYxNGg2Ljd2LTIxaC0zOHpNODUuMyA3aDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3ptMCAxMy44aDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3ptMCAxNC4xaDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3pNMzA4LjUgN2gyNmE5LjYgOS42IDAgMCAwIDctN2gtNDBhOS42IDkuNiAwIDAgMCA3IDd6IiBmaWxsPSJjdXJyZW50Q29sb3IiLz48L3N2Zz4="
+            alt="Tesla Logo"
+          />
+        </a>
       </div>
       <div className="hidden lg:flex">
         <ul className="flex items-center justify-center space-x-6 cursor-pointer">
-          <li className="hover-rounded hover:bg-black/5">Model S</li>
-          <li className="hover-rounded hover:bg-black/5">Model 3</li>
-          <li className="hover-rounded hover:bg-black/5">Model X</li>
-          <li className="hover-rounded hover:bg-black/5">Model Y</li>
-          <li className="hover-rounded hover:bg-black/5">Solar Roof</li>
-          <li className="hover-rounded hover:bg-black/5">Solar Panels</li>
+          <li className="hover-rounded hover:bg-black/5">
+            <a href="https://www.tesla.com/models">Model S</a>
+          </li>
+          <li className="hover-rounded hover:bg-black/5">
+            <a href="https://www.tesla.com/model3">Model 3</a>
+          </li>
+          <li className="hover-rounded hover:bg-black/5">
+            <a href="https://www.tesla.com/modelx">Model X</a>
+          </li>
+          <li className="hover-rounded hover:bg-black/5">
+            <a href="https://www.tesla.com/modely">Model Y</a>
+          </li>
+          <li className="hover-rounded hover:bg-black/5">
+            <a href="https://www.tesla.com/solarroof">Solar Roof</a>
+          </li>
+          <li className="hover-rounded hover:bg-black/5">
+            <a href="https://www.tesla.com/solarpanels">Solar Panels</a>
+          </li>
         </ul>
       </div>
       <div className="hidden lg:flex">
         <ul className="flex items-center justify-center space-x-6 cursor-pointer">
-          <li className="hover-rounded hover:bg-black/5">Shop</li>
-          <li className="hover-rounded hover:bg-black/5">Account</li>
+          <li className="hover-rounded hover:bg-black/5">
+            <a href="https://shop.tesla.com/">Shop</a>
+          </li>
+          <li className="hover-rounded hover:bg-black/5">
+            <a href="https://www.tesla.com/teslaaccount">Account</a>
+          </li>
           <li className="hover-rounded hover:bg-black/5">Menu</li>
         </ul>
       </div>
 
       {/* mobile menu button */}
       <div className="lg:hidden">
-        <button className="inline-flex justify-center items-center rounded-md py-2 px-4 text-sm font-medium bg-black/5 shadow-sm hover:bg-black/10">Menu</button>
+        <button
+          onClick={handleNav}
+          className="inline-flex justify-center items-center rounded-md py-2 px-4 text-sm font-medium bg-black/5 shadow-sm hover:bg-black/10"
+        >
+          Menu
+        </button>
+      </div>
+
+      {/* sidebar */}
+      <div
+        className={
+          nav
+            ? "bg-white absolute top-0 right-0 w-80 h-full z-10"
+            : "fixed right-[-100%]"
+        }
+      >
+        <div className="flex justify-end pr-8 pt-8">
+          <svg
+            onClick={handleNav}
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-8 h-8 cursor-pointer rounded p-1 hover:bg-black/5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </div>
+        <ul className="pt-8 px-6">
+          <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
+            <a href="https://tesla.com/models">Model S</a>
+          </li>
+          <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
+            <a href="https://tesla.com/model3">Model 3</a>
+          </li>
+          <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
+            <a href="https://tesla.com/modelx">Model X</a>
+          </li>
+          <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
+            <a href="https://tesla.com/modely">Model Y</a>
+          </li>
+          <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
+            <a href="https://tesla.com/solarroof">Solar Roof</a>
+          </li>
+          <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
+            <a href="https://tesla.com/solarpanels">Solar Panels</a>
+          </li>
+          <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
+            <a href="https://www.tesla.com/inventory/new/m3">
+              Existing Inventory
+            </a>
+          </li>
+          <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
+            <a href="https://www.tesla.com/inventory/used/m3">Used Inventory</a>
+          </li>
+          <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
+            <a href="https://www.tesla.com/tradein">Trade-In</a>
+          </li>
+          <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
+            <a href="https://www.tesla.com/drive">Test Drive</a>
+          </li>
+          <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
+            <a href="https://www.tesla.com/insurance">Insurance</a>
+          </li>
+          <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
+            <a href="https://www.tesla.com/powerwall">Powerwall</a>
+          </li>
+        </ul>
       </div>
     </div>
   );
